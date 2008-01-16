bacb19c7fb8647d3efe2ca8129368640c3258ca5
corvus <corvus@8a1616d9-aaba-4d60-a407-a19f20b05aef>
2008-01-16 06:50:53 +0000

    Bleh, going to bed.  Wired fryed.  No clue why num_rows isn't returning what its supposed to

--- db-dist.php
+++ db-dist.php
@@ -5,4 +5,11 @@ $username = "{$_SERVER['PHP_AUTH_USER']}";
 // Connect to MySQL
 $mysqli = new mysqli('host','username','password','db')
 	or die("Problem connecting:".mysqli_error());
+
+// We have a seperate database for our game info (goes under the account admin tools)
+if (!$authmysqli) {
+	        $authmysqli = $mysqli;
+}
+
+
 ?> 
--- index.php
+++ index.php
@@ -1,15 +1,40 @@
 <?
-include db.php
-include header.php
+include "db.php";
 
-print "Hello $username to the Downtime Manager\n";
+$mode = $mysqli->real_escape_string($_POST['mode']);
+//$username = $mysqli->real_escape_string($_POST['username']);
 
-// Looking for the cookie to set the game and PC used
-if (!$_COOKIE['game']) {
-	print "I see you have not chosen a game yet.\n";
-	}
-else {
-	print "You are working with $_COOKIE['pcname'] in $_COOKIE['game'].  To fix that go <a href="pcsettings.php">here</a>\n";
+switch ($mode) {
+default:
+	print "Hello $username to the Downtime Manager!<br>\n";
+
+	if (!$_COOKIE['pcid']) {
+		print "Ah, I see that you have not picked your character.  Lets fix that.<br>\n";
+		print "<form action=\"index.php\" method=\"post\">\n";
+		print "<select name=\"pcid\">\n";
+		$getuserid = $authmysqli->prepare("SELECT userid FROM users WHERE username=\"$username\"");
+		$getuserid->execute();
+		$getuserid->bind_result($userid);
+		$getuserid->fetch();
+		$getpcid = $mysqli->prepare("SELECT pcid,name FROM players WHERE userid=\"$userid\"");
+		$getpcid->execute();
+		$rows = $getpcid->num_rows();
+		print "$userid $rows";
+		if ($getpcid->num_rows > 0) {
+			$getpcid->bind_result($pcid,$pcname);
+			while ($getpcid->fetch()) {
+				print "<option value=\"$pcid\">$pcname</option>\n";
+			}
+		}		
+		print "<option value=\"createnew\">Create New PC</option>\n";
+		print "</select>\n";
+ 		print "<input type=\"submit\" value=\"Select PC\">\n";
+ 	}
+
+	break;
 }
+
+
+
 
 ?>
