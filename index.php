0ff87ae6538aef3d2c7988bafdba1f45dd255ad9
corvus <corvus@8a1616d9-aaba-4d60-a407-a19f20b05aef>
2008-01-16 20:49:38 +0000

    Added basic functions for cookie making/destroying/etc

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 include "db.php";
 
 $mode = $mysqli->real_escape_string($_POST['mode']);
+$pcid = $mysqli->real_escape_string($_POST['pcid']);
 //$username = $mysqli->real_escape_string($_POST['username']);
 
 switch ($mode) {
@@ -32,13 +33,50 @@ default:
 		}			
 		print "<option value=\"createnew\">Create New PC</option>\n";
 		print "</select>\n";
+ 		print "<input type=\"mode\" value=\"setcookie\">\n";
  		print "<input type=\"submit\" value=\"Select PC\">\n";
  	}
-
+	if ($_COOKIE['pcid'] ) {
+		print "Ah, you are here for $pcid\n";
+	}
 	break;
+
 }
 
 
+function createpcidcookie($pcid) {
+	setcookie("pcid","$pcid",mktime()+86400,"/") or die("Could not set cookie");
+	if ($_COOKIE['pcid']) {
+		print "Cookie set!";
+		return;
+	}
+}
 
 
+function deletepcidcookie($pcid) {
+	setcookie("pcid","$pcid",mktime()-86400,"/") or die("Could not delete cookie");
+	if (!$_COOKIE['pcid']) {
+		print "Cookie deleted!";
+		return;
+	}
+}
+
+function checkpcidcookie($pcid,$username) {
+	$getuserid = $authmysqli->prepare("SELECT userid FROM users WHERE username=?");
+	$getuserid->bind_param('s',$username);
+	$getuserid->execute();
+	if ($getuserid) {
+		$getuserid->bind_result($userid);
+		$getuserid->fetch();
+		// Use that userid to find out what characters they are playing
+		$getpcid = $mysqli->prepare("SELECT pcid,name FROM players WHERE userid=? AND pcid=\"$pcid\"");
+		$getpcid->bind_param('i',$userid);
+		$getpcid->execute();
+		if (!$getpcid) {
+			print "Forgery of cookie!<br>\n";
+			deletepcidcookie($pcid);
+			return;
+		}
+
+}
 ?>
