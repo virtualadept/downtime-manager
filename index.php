60fd86d0b1e9bd2afec3a39f5e809e7527639918
corvus <corvus@8a1616d9-aaba-4d60-a407-a19f20b05aef>
2008-01-16 15:52:40 +0000

    This looks kludgy, but I think its the 'right' way to do it.

--- index.php
+++ index.php
@@ -6,26 +6,30 @@ $mode = $mysqli->real_escape_string($_POST['mode']);
 
 switch ($mode) {
 default:
-	print "Hello $username to the Downtime Manager!<br>\n";
-
+	print "Hello $username to the Downtime Manager!<br><br><br>\n";
+	// If they dont have a cookie set, ask them which character they want.
 	if (!$_COOKIE['pcid']) {
 		print "Ah, I see that you have not picked your character.  Lets fix that.<br>\n";
 		print "<form action=\"index.php\" method=\"post\">\n";
 		print "<select name=\"pcid\">\n";
-		$getuserid = $authmysqli->prepare("SELECT userid FROM users WHERE username=\"$username\"");
+		// Pull the userid of the logged in user from the apacheauth db.
+		$getuserid = $authmysqli->prepare("SELECT userid FROM users WHERE username=?");
+		$getuserid->bind_param('s',$username);
 		$getuserid->execute();
-		$getuserid->bind_result($userid);
-		$getuserid->fetch();
-		$getpcid = $mysqli->prepare("SELECT pcid,name FROM players WHERE userid=\"$userid\"");
-		$getpcid->execute();
-		$rows = $getpcid->num_rows();
-		print "$userid $rows";
-		if ($getpcid->num_rows > 0) {
-			$getpcid->bind_result($pcid,$pcname);
-			while ($getpcid->fetch()) {
-				print "<option value=\"$pcid\">$pcname</option>\n";
+		if ($getuserid) {
+			$getuserid->bind_result($userid);
+			$getuserid->fetch();
+			// Use that userid to find out what characters they are playing
+			$getpcid = $mysqli->prepare("SELECT pcid,name FROM players WHERE userid=?");
+			$getpcid->bind_param('i',$userid);
+			$getpcid->execute();
+			if ($getpcid) {
+				$getpcid->bind_result($pcid,$pcname);
+				while ($getpcid->fetch()) {
+					print "<option value=\"$pcid\">$pcname</option>\n";
+				}
 			}
-		}		
+		}			
 		print "<option value=\"createnew\">Create New PC</option>\n";
 		print "</select>\n";
  		print "<input type=\"submit\" value=\"Select PC\">\n";
