a5fdfd76dad98ac0a0777e8ad827b6d34529187d
corvus <corvus@8a1616d9-aaba-4d60-a407-a19f20b05aef>
2008-01-17 23:53:11 +0000

    Inital import/roadmap for pc.php (PC editing interface)

---
+++ pc.php
@@ -0,0 +1,11 @@
+<?
+
+/*
+* Read pcid from cookie if not pull-down list of characters
+* Email maintence, name maintence
+* Allows character to 'join' or 'leave' a game.
+* Confirmation
+* Emails ST with change
+*/
+
+?>
