4020e548dac82b54ad4e6ce5788291ec02c5a5a5
corvus <corvus@8a1616d9-aaba-4d60-a407-a19f20b05aef>
2008-01-17 19:56:06 +0000

    Inital import/plan of attack for the st interface

---
+++ st.php
@@ -0,0 +1,17 @@
+<?
+include "db.php";
+
+if (!$_COOKIE['st']) {
+	print "You dont seem to have a game you ST set.<br>\n";
+}
+
+// Get userid from username
+// Get list of games that userid ST's
+// Print list
+// Set cookie
+// Show players involved and characters involved in game.
+// Add/Edit/Delete users access to games.
+// ? Add/edit/del players in a game.
+
+
+?>
