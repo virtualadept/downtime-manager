838d4f01f6dbd73063d88c8f44b028c3e9e813df
corvus <corvus@8a1616d9-aaba-4d60-a407-a19f20b05aef>
2008-01-17 19:40:19 +0000

    This isn't goign to be a huge one-file case-driven monster clusterfuck of a program. We are going to break it up into player.php and st.php
    index.php is where the downtime summaries will be

--- index.php
+++ index.php
@@ -6,59 +6,54 @@ $pcid = $mysqli->real_escape_string($_POST['pcid']);
 $cookie = $mysqli->real_escape_string($_POST['cookie']);
 //$username = $mysqli->real_escape_string($_POST['username']);
 
-switch ($mode) {
-default:
-	if ($cookie == 'set') {
-		setpcidcookie($pcid);
-	}
-	print "Hello $username to the Downtime Manager!<br><br><br>\n";
-	// If they dont have a cookie set, ask them which character they want.
-	if (!$_COOKIE['pcid']) {
-		print "Ah, I see that you have not picked your character.  Lets fix that.<br>\n";
-		print "<form action=\"index.php\" method=\"post\">\n";
-		print "<select name=\"pcid\">\n";
-		// Pull the userid of the logged in user from the apacheauth db.
-		$getuserid = $authmysqli->prepare("SELECT userid FROM users WHERE username=?");
-		$getuserid->bind_param('s',$username);
-		$getuserid->execute();
-		if ($getuserid) {
-			$getuserid->bind_result($userid);
-			$getuserid->fetch();
-			// Use that userid to find out what characters they are playing
-			$getpcid = $mysqli->prepare("SELECT pcid,name FROM players WHERE userid=?");
-			$getpcid->bind_param('i',$userid);
-			$getpcid->execute();
-			if ($getpcid) {
-				$getpcid->bind_result($pcid,$pcname);
-				while ($getpcid->fetch()) {
-					print "<option value=\"$pcid\">$pcname</option>\n";
-				}
-			}
-		}			
-		print "<option value=\"createnew\">Create New PC</option>\n";
-		print "</select>\n";
- 		print "<input type=\"hidden\" name=\"cookie\" value=\"set\">\n";
- 		print "<input type=\"submit\" value=\"Select PC\">\n";
- 	}
-	if ($_COOKIE['pcid'] ) {
-		$cookiepcid = $_COOKIE['pcid'];
-		$getplayername = $mysqli->prepare("SELECT name FROM players WHERE pcid=?");
-		$getplayername->bind_param('i',$cookiepcid);
-		$getplayername->execute();
-		$getplayername->bind_result($playername);
-		$getplayername->fetch();
-		print "Ah, You are here for $playername, excellent!<br>\n";
-	}
-	
-	if ($_COOKIE['pcid'] && !$_COOKIE['gameid']) {
-		print "You have not chosen a game yet! Lets fix that:\n";
-
-	}
-	
-	
-	break;
-
+if ($cookie == 'set') {
+	setpcidcookie($pcid);
 }
+print "Hello $username to the Downtime Manager!<br><br><br>\n";
+
+// If they dont have a cookie set, ask them which character they want.
+if (!$_COOKIE['pcid']) {
+	print "Ah, I see that you have not picked your character.  Lets fix that.<br>\n";
+	print "<form action=\"index.php\" method=\"post\">\n";
+	print "<select name=\"pcid\">\n";
+	// Pull the userid of the logged in user from the apacheauth db.
+	$getuserid = $authmysqli->prepare("SELECT userid FROM users WHERE username=?");
+	$getuserid->bind_param('s',$username);
+	$getuserid->execute();
+	if ($getuserid) {
+		$getuserid->bind_result($userid);
+		$getuserid->fetch();
+		// Use that userid to find out what characters they are playing
+		$getpcid = $mysqli->prepare("SELECT pcid,name FROM players WHERE userid=?");
+		$getpcid->bind_param('i',$userid);
+		$getpcid->execute();
+		if ($getpcid) {
+			$getpcid->bind_result($pcid,$pcname);
+			while ($getpcid->fetch()) {
+				print "<option value=\"$pcid\">$pcname</option>\n";
+			}
+		}
+	}			
+	print "<option value=\"createnew\">Create New PC</option>\n";
+	print "</select>\n";
+		print "<input type=\"hidden\" name=\"cookie\" value=\"set\">\n";
+		print "<input type=\"submit\" value=\"Select PC\">\n";
+	}
+
+if ($_COOKIE['pcid'] ) {
+	$cookiepcid = $_COOKIE['pcid'];
+	$getplayername = $mysqli->prepare("SELECT name FROM players WHERE pcid=?");
+	$getplayername->bind_param('i',$cookiepcid);
+	$getplayername->execute();
+	$getplayername->bind_result($playername);
+	$getplayername->fetch();
+	print "Ah, You are here for $playername, excellent!<br>\n";
+}
+	
+if ($_COOKIE['pcid'] && !$_COOKIE['gameid']) {
+	print "You have not chosen a game yet! Lets fix that:\n";
+}
+	
 
 function getuseridfromusername($username) {
 	$getuserid = $authmysqli->prepare("SELECT userid FROM users WHERE username=?");
