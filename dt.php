2ca38acb85b8e64da3a191227c5e15d87b82e0b5
corvus <corvus@8a1616d9-aaba-4d60-a407-a19f20b05aef>
2008-01-17 23:57:18 +0000

    Inital import and roadmap for dt.php (downtime event viewer)

---
+++ dt.php
@@ -0,0 +1,12 @@
+<?
+
+/* Brings up the downtime ID and relevant conversations
+* Allows one to add/change/delete conversation
+* Only allows participating + ST to view downtimes
+* Allows closure of DT's
+* Upon finishing a DT, makes a wiki compatable output
+* Emails out everyone when a downtime is updated/changed
+*/
+
+
+?>
