6b6b5d0193f5f0cec3d8fb7868d3038c54587e20
corvus <corvus@8a1616d9-aaba-4d60-a407-a19f20b05aef>
2008-01-20 01:23:12 +0000

    Edit/save people who have access to the game

--- st.php
+++ st.php
@@ -1,8 +1,9 @@
 <?
 include "include.php";
 
-$gameid = $mysqli->real_escape_string($_POST['gameid']);
-$cookie = $mysqli->real_escape_string($_POST['cookie']);
+$gameid = $mysqli->real_escape_string($_GET['gameid']);
+$cookie = $mysqli->real_escape_string($_GET['cookie']);
+$mode = $mysqli->real_escape_string($_POST['mode']);
 $userslist = ($_POST['userslist']);
 
 if ($cookie == "set" && $gameid) {
@@ -12,7 +13,7 @@ if ($cookie == "set" && $gameid) {
 if (!$_COOKIE['st']) {
 	$userid = getuseridfromusername($authmysqli,$username);
 	if ($getstlist = $mysqli->prepare("SELECT gameid,name FROM games WHERE stuserid=?")) {
-		print "<form action=\"st.php\" method=\"post\">\n";
+		print "<form action=\"st.php\" method=\"get\">\n";
 		print "<select name=\"gameid\">\n";
 		$getstlist->bind_param('i',$userid);
 		$getstlist->execute();
@@ -33,42 +34,54 @@ if ($_COOKIE['st']) {
 	$cookiegameid = $_COOKIE['st'];
 	$userid = getuseridfromusername($authmysqli,$username);
 	$gameinfo = getgameinfofromgameid($mysqli,$cookiegameid,$userid);
+
+
+	if ($mode == "userset" && $userslist) {
+		// Check to see if their cookie is legit
+		$userid = getuseridfromusername($authmysqli,$username);
+		$cookiegameid = $_COOKIE['st'];
+		$gameinfo = getgameinfofromgameid($mysqli,$cookiegameid,$userid);
+		if (!$gameinfo) {
+			print "Warning! Some cookie douchebaggery is going on here!<br>\n";
+		//	exit;
+		}
+		// First, we'll just delete everyone out of the DB who is a part of that game
+		$deleteaccessusers = $mysqli->query("DELETE FROM access WHERE gameid=\"$cookiegameid\" AND type='U'");
+		// If we are at this point, all went well, lets add people back.
+		if ($addaccessusers = $mysqli->prepare("INSERT INTO access (gameid,type,id) VALUES (?,'U',?)")) {
+			foreach ($userslist as $entereduserid) {
+				$addaccessusers->bind_param('ii',$cookiegameid,$entereduserid);
+				$addaccessusers->execute();
+			}
+			print "Done!<br><br>\n";
+		}
+	}
+
+
 	print "You are the ST for " . $gameinfo["name"] . "<br>\n";
 	print "Please select which people are a part of your game:<br>\n";
+	print "<form action=\"st.php\" method=\"post\">\n";
 	// User list.  First we'll nab from the DB all of the people who are in the game
-	if ($getactive = $mysqli->prepare("SELECT id FROM access WHERE gameid=? AND id=\"U\"")) {
+	if ($getactive = $mysqli->prepare("SELECT id FROM access WHERE gameid=? AND type=\"U\"")) {
 		$getactive->bind_param('i',$cookiegameid);
 		$getactive->execute();
 		$getactive->bind_result($activeuser);
 		while ($getactive->fetch()) {
-			$activehash["$activeuser"] = 1;
+			$activehash[$activeuser] = "present";
 		}
 	}
 	if ($getuserslist = $authmysqli->query("SELECT userid,username FROM users ORDER BY username ASC")) {
 		while ($users = $getuserslist->fetch_assoc()) {
-			$out = "<input type=\"checkbox\" name=\"userlists[]\" value=\"".$users['userid']."\"";
-			if (exists($activehash[$users['userid'])) {
-				$out =. " checked ";
+			$out = "<input type=\"checkbox\" name=\"userslist[]\" value=\"".$users['userid']."\"";
+			if (isset($activehash[$users['userid']])) {
+				$out .= " checked ";
 			}
-			$out =. ">".$users['username']."<br>";"
+			$out .= ">".$users['username']."<br>";
 			print "$out\n";
 		}
 	print "<input type=\"hidden\" name=\"mode\" value=\"userset\">\n";
 	print "<input type=\"submit\" value=\"Go\">\n";
 	}
-
-	if ($mode == "userset") {
-		// Check to see if their cookie is legit
-		$userid = getuseridfromusername($authmysqli,$username);
-		$cookiegameid = $_COOKIE['st'];
-		$gameinfo = getgameinfofrom gameid($mysqli,$cookiegameid,$userid);
-		if (!$gameinfo) {
-			print "Warning! Some cookie douchebaggery is going on here!<br>\n";
-			exit;
-		}
-	}
-
-
 
 }
 
