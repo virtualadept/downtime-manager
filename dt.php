fc6504a376444e3d533eaddfa300ecd8e33a9467
corvus <corvus@8a1616d9-aaba-4d60-a407-a19f20b05aef>
2008-01-23 19:52:11 +0000

    Framework for downtime creation/viewing

--- dt.php
+++ dt.php
@@ -19,13 +19,19 @@ if ($_COOKIE['pcid'] && $_COOKIE['gameid']) {
 	exit;
 }
 
-if ($getopendt = $mysqli->query("SELECT dtid,createdate,name FROM dtmeta,games WHERE pcid=\"$pcid\" AND dtmeta.gameid=\"$gameid\" AND dtmeta.status=\"O\" AND games.gameid = dtmeta.gameid");
+if ($getopendt = $mysqli->query("SELECT dtid,createdate,name FROM dtmeta,games WHERE pcid=\"$pcid\" AND dtmeta.gameid=\"$gameid\" AND dtmeta.status=\"O\" AND games.gameid = dtmeta.gameid SORT BY createdate ASC LIMIT 10");
 	print "These are the latest 10 open downtimes that you own<br>\n"
 	while($opendt = $getopendt->fetch_array()) {
 		print "id: $opendt['dtid'] - $opendt['name'] opened $opendt['createdate']<br>\n";
 	}
 }
-	
 
+// Create a new downtime
+if (!$dtid && $mode == 'create') {
 
+}
+// Do things with an existing dt
+if ($dtid) {
+
+}
 ?>
